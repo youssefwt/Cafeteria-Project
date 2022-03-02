39cabb8ae79d9f727e44d0bd5e70fea1c2963bd9
unknown <azharelamyr@gmail.com>
2022-03-03 00:49:01 +0200

    addProduct

---
+++ php/Product_Validation.php
@@ -0,0 +1,52 @@
+<?php
+
+$errors =[];
+
+if (empty($_REQUEST["productName"])&& $_REQUEST["productName"]=="") {
+    $errors['productName']="productName_is_empty";
+}
+var_dump($_FILES);
+$file_name = $_FILES['img']['name'];
+ var_dump($file_name);
+$file_tmp =$_FILES['img']['tmp_name'];
+$file_type=$_FILES['img']['type'];
+
+$extention = explode("/", $file_type)[1];
+
+$extensions= array("jpeg","jpg","png", "gif");
+
+if (in_array($extention, $extensions)=== false) {
+    $errors["file"]="extension not allowed, please choose a JPEG, jpg, gif or PNG file.";
+}
+
+$str="addProduct.php?";
+if (count($errors)>0) {
+    foreach ($errors as $k=>$val) {
+        $str.=$k."=".$val."&";
+    }
+    header("Location: $str");
+    return;
+}
+
+echo"<br>";
+
+
+$the_picture = $_REQUEST["productName"].".".$extention;
+var_dump($the_picture);
+
+
+move_uploaded_file($file_tmp, "../assets/images/test-images/".$_REQUEST["productName"].".".$extention);
+
+include("dbManager.php");
+
+$db = new DbManager();
+
+try {
+   
+    $db->addProduct($_REQUEST['productName'], $_REQUEST['Price'], $the_picture, $_REQUEST['status']);
+    header("Location: product_table.php");
+   
+} catch (PDOException $e) {
+    echo 'Connection failed: ' . $e->getMessage();
+}
+?>
--- php/dbManager.php
+++ php/dbManager.php
@@ -3,8 +3,10 @@
 class DbManager
 {
     private $dsn = 'mysql:dbname=CafeteriaDB;host=127.0.0.1;port=3306;';
-    private $user = 'admin';
-    private $password = '12345678';
+    // private $user = 'admin';
+    // private $password = '12345678';
+    private $user = 'root';
+    private $password = 'Azhar254@';
     public $pdo;
 
     public function __construct()
@@ -33,5 +35,41 @@ class DbManager
         $stmt->closeCursor();
         $myjson = json_encode($stds);
         echo $myjson;
+    }
+
+    public function addProduct(...$args){
+        $query = "INSERT INTO `products` (`name`, `Price`, `image_url`, `status`) VALUES(?,?,?,?)";
+        $stmt = $this->pdo->prepare($query);
+        $stmt->execute($args);
+    }
+    public function Product_table($table_name, ...$args){
+      echo" <div class='container w-50 mt-5 border rounded-3'>"  ;
+        echo "<table class='table table-striped'><tr>";
+        for($i = 0; $i < count($args);$i++){
+            echo"<th scope='col'>$args[$i]</th>";
+        }
+        
+        echo '<th>Image</th>';
+        echo '<th colspan="2">Action</th>';
+        echo '</tr>';
+        $query = "SELECT * FROM $table_name;";
+        $stmt=$this->pdo->prepare($query);
+        $stmt->execute();
+        while ($obj = $stmt -> fetchObject()) {
+            echo '<tr>';
+            for ($i = 0; $i < count($args);$i++){
+                echo '<td>';
+                $something = $args[$i];
+                echo $obj->$something;
+                echo '</td>';
+            }
+            echo "<td><img src='../assets/images/test-images/$obj->image_url' style='width:50px;height:50px'></td>";
+            
+            echo "<td><a href='edit.php?id=".$obj->id."'>Edit</a></td>";
+            echo "<td><a href='delete.php?id=".$obj->id."'>Delete</a></td>";
+            echo'</tr>';
+        }
+        echo '</table>';
+       echo "</div>" ;
     }
 }
